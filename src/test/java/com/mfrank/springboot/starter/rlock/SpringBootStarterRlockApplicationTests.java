c6789a628fe6c2a5020628fd232a6c3aa36ff798
mfrank <yingjia.qiu@ximalaya.com>
2019-10-17 15:03:40 +0800

    first commit

---
+++ src/test/java/com/mfrank/springboot/starter/rlock/SpringBootStarterRlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfrank.springboot.starter.rlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterRlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
